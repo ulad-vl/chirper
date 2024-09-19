422f3c454704aa0a60e3071d13c5d14d6db39bdb
Uladzimir <vlhardy@triplenext.com>
2024-09-19 20:55:27 +0200

    - configure clustering and state to use Azure table storage - modify manifests to deploy to k8s cluster

--- Chirper.Client/Program.cs
+++ Chirper.Client/Program.cs
@@ -2,12 +2,24 @@ using Chirper.Client;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+using Azure.Data.Tables;
 
 Console.Title = "Chirper Client";
 
 await new HostBuilder()
-    .UseOrleansClient(builder =>
-        builder.UseLocalhostClustering())
+    .UseOrleansClient(clientBuilder =>
+    {
+        clientBuilder.UseAzureStorageClustering(options =>
+                    {
+                        options.TableServiceClient = new TableServiceClient("UseDevelopmentStorage=true;");
+                    })
+                    .Configure<ClusterOptions>(options =>
+                    {
+                        options.ClusterId = "chirper-cluster";
+                        options.ServiceId = "chirper-service";
+                    });
+    })
     .ConfigureServices(
         services => services
             .AddSingleton<IHostedService, ShellHostedService>()
--- Chirper.Server/Program.cs
+++ Chirper.Server/Program.cs
@@ -1,19 +1,39 @@
 // chirper server
 
 using Microsoft.Extensions.Hosting;
+using Azure.Data.Tables;
+using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
 
 Console.Title = "Chirper Server";
 
 var builder = Host.CreateDefaultBuilder(args)
-    .UseOrleans(
-        builder => builder
-            .UseLocalhostClustering()
-            .AddMemoryGrainStorage("AccountState")
+    .UseOrleans(siloBuilder =>
+    {
+        siloBuilder
+            .UseAzureStorageClustering(options =>
+            {
+                options.TableServiceClient = new TableServiceClient("UseDevelopmentStorage=true;");
+                //options.TableName = "ClusterMembershipTable";
+            })
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = "chirper-cluster";
+                options.ServiceId = "chirper-service";
+            })
+            .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
+            .AddAzureTableGrainStorage("AccountState", options =>
+            {
+                options.TableServiceClient = new TableServiceClient("UseDevelopmentStorage=true;");
+                options.TableName = "AccountState";
+            })
             .UseDashboard(options =>
             {
                 options.Port = 8080;
                 //options.BasePath = "/dashboard";
-            })
-            );
+            });
+    })
+    .ConfigureLogging(logging => logging.AddConsole());
+
 
 await builder.RunConsoleAsync();
